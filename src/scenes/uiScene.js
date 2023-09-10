9f75e71db6c04f9f823d8554c299484b09d80985
randomnewb <nguy1581@gmail.com>
2023-09-09 20:23:27 -0500

    broke restartGame, added custom font

--- src/scenes/uiScene.js
+++ src/scenes/uiScene.js
@@ -1,5 +1,4 @@
 import data from "../data/data.js";
-import InputText from "phaser3-rex-plugins/plugins/inputtext.js";
 
 export default class UIScene extends Phaser.Scene {
   constructor() {
@@ -7,68 +6,80 @@ export default class UIScene extends Phaser.Scene {
   }
 
   create() {
-    // access phaser 3 js config file
     this.gameWidth = this.game.config.width;
     this.gameHeight = this.game.config.height;
 
     this.gameOverText = this.add.text(
       1280 / 2,
       720 / 2,
-      "Game Over\nPress R to Restart",
+      "Game Over\n\nPlease Generate a\n New World to Play Again",
       {
         fontSize: "80px",
         fill: "#fff",
         align: "center",
+        fontFamily: "HopeGold",
       }
     );
     this.gameOverText.setOrigin(0.5);
     this.gameOverText.setVisible(false);
 
-    this.input.keyboard.on("keydown-R", this.restartGame, this);
+    this.restartButton = this.add
+      .text(35, 50, " Generate World ", {
+        fontSize: "25px",
+        fill: "black",
+        align: "center",
+        fontFamily: "HopeGold",
+        backgroundColor: "lightblue",
+      })
+      .setInteractive({ useHandCursor: true })
+      .on("pointerover", () => {
+        this.restartButton.setStyle({ fill: "#3d5a36" });
+      })
+      .on("pointerout", () => {
+        this.restartButton.setStyle({ fill: "black" });
+      })
+      .on("pointerdown", () => {
+        this.restartButton.setStyle({ fill: "#2e2740" });
+      })
+      .on("pointerup", () => {
+        this.restartGame();
+        this.restartButton.setStyle({ fill: "#10434e" });
+      })
+      .setVisible(true);
 
     this.create_input_field();
-    this.create_restart_button();
+    this.restartButton.visible = true;
 
     this.inputText.on(
       "keydown",
       function (inputText, e) {
         if (e.key === "Enter") {
-          data.gameSeed = this.inputText.text;
-
-          this.events.emit("generate");
-
-          this.inputText.text = "";
-          this.inputText.visible = false;
-          this.inputText.setBlur();
+          this.restartGame();
         }
       },
       this
     );
-
-    // this.inputText.on("keydown", event => {
-    //   if (event.key === "Enter") {
-    //     data.gameSeed = this.input.value;
-
-    //     this.events.emit("generate");
-
-    //     this.inputText.text = "";
-    //   }
-    // });
   }
 
   showGameOver() {
     this.gameOverText.setVisible(true);
     this.inputText.visible = true;
-    this.button.style.display = "block";
+    this.restartButton.visible = true;
   }
 
   hideGameOver() {
     this.gameOverText.setVisible(false);
-    this.button.style.display = "none";
+    this.inputText.visible = true;
   }
 
   restartGame() {
     if (!data.gameActive) {
+      data.gameSeed = this.inputText.text;
+
+      this.inputText.text = "";
+      this.inputText.setBlur();
+      this.events.emit("generate");
+
       data.gameActive = true;
 
       const gameScene = this.scene.get("GameScene");
@@ -99,32 +110,9 @@ export default class UIScene extends Phaser.Scene {
       backgroundColor: "lightblue",
       color: "black",
       align: "center",
+      fontFamily: "HopeGold",
     });
 
     // this.inputText.node.style.backgroundColor = "lightblue";
-  }
-
-  create_restart_button() {
-    // create a button and add it to the DOM
-    this.button = document.createElement("button");
-    this.button.innerHTML = "Restart";
-    document.body.appendChild(this.button);
-
-    // set the button position
-    const canvas = document.querySelector("canvas");
-    const canvasPosition = canvas.getBoundingClientRect();
-    this.button.style.position = "absolute";
-    this.button.style.top = `${canvasPosition.top}px`;
-    this.button.style.left = `${canvasPosition.left * 100.0}px`;
-
-    this.button.style.width = "250px";
-    this.button.style.height = "50px";
-
-    // add a listener to the button
-    this.button.addEventListener("click", () => {
-      this.restartGame();
-    });
-
-    // this.button.style.display = "none";
   }
 }
